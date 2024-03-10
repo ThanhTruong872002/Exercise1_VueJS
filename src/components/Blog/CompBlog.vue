bed0cd1b5258ef494d50cde6eca0e5d63ae8c231
School <118030762+nguyenduyquang0@users.noreply.github.com>
2024-03-10 23:38:13 +0700

    fix bug

--- src/components/Blog/CompBlog.vue
+++ src/components/Blog/CompBlog.vue
@@ -54,23 +54,23 @@ export default {
     return {
       brand: [
         {
-          image: "/src/images/Brand1.svg",
+          image: "./src/images/Brand1.svg",
           link: "https://www.atlassian.com/git/tutorials/syncing/git-pull",
         },
         {
-          image: "/src/images/Brand2.svg",
+          image: "./src/images/Brand2.svg",
           link: "https://www.bing.com/search?pglt=41&q=slack&cvid=c9980da298bb43be807f911bab27e39b&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQABhAMgYIAhAAGEAyBggDEAAYQDIGCAQQABhAMgYIBRAAGEAyBggGEAAYQDIGCAcQABhAMgYICBAAGEDSAQg0MzcxajBqMagCALACAA&FORM=ANNTA1&adppc=EDGEESS&PC=EDGEESS",
         },
         {
-          image: "/src/images/Brand3.svg",
+          image: "./src/images/Brand3.svg",
           link: "https://www.dropbox.com/",
         },
         {
-          image: "/src/images/Brand4.svg",
+          image: "./src/images/Brand4.svg",
           link: "https://www.shopify.com/free-trial/3s?gclid=17bfd3e6b02314f2b192b60a8cc9c074&gclsrc=3p.ds&&utm_medium=cpc&utm_source=yabing&jk=shopify&utm_source=yabing&utm_medium=cpc&utm_campaign=429206062&bingadgroupid=1230353764689029&bingadid=76897258418057&bingkeywordid=76897314828284&bingnetwork=o&BOID=brand&msclkid=17bfd3e6b02314f2b192b60a8cc9c074&utm_term=shopify&utm_content=Brand%20-%20Shopify",
         },
         {
-          image: "/src/images/Brand5.svg",
+          image: "./src/images/Brand5.svg",
           link: "https://www.google.com/",
         },
       ],
@@ -79,19 +79,19 @@ export default {
           title: "design",
           description: "Truly it was a great journey and in it I met with many",
           date: "14 November, 2019",
-          image: "/src/images/block1.svg",
+          image: "./src/images/block1.svg",
         },
         {
           title: "tech",
           description: "What looked like a small patch of purple grass",
           date: "14 November, 2019",
-          image: "/src/images/block2.svg",
+          image: "./src/images/block2.svg",
         },
         {
           title: "life",
           description: "The roots were revolving small plant",
           date: "14 November, 2019",
-          image: "/src/images/block3.svg",
+          image: "./src/images/block3.svg",
         },
       ],
     };
