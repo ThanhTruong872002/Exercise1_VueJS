7b147bb5bf3cd6d7bff35f343a860bba142b6bae
School <118030762+nguyenduyquang0@users.noreply.github.com>
2024-03-12 09:53:40 +0700

    update

--- src/constants/Blog.js
+++ src/constants/Blog.js
@@ -1,20 +1,24 @@
+import blog1 from "../assets/images/block1.svg";
+import blog2 from "../assets/images/block2.svg";
+import blog3 from "../assets/images/block3.svg";
+
 export const BLOGS = [
   {
     title: "design",
     description: "Truly it was a great journey and in it I met with many",
     date: "14 November, 2019",
-    image: "./src/assets/images/Block1.svg",
+    image: blog1,
   },
   {
     title: "tech",
     description: "What looked like a small patch of purple grass",
     date: "14 November, 2019",
-    image: "./src/assets/images/Block2.svg",
+    image: blog2,
   },
   {
     title: "life",
     description: "The roots were revolving small plant",
     date: "14 November, 2019",
-    image: "./src/assets/images/Block3.svg",
+    image: blog3,
   },
 ];
